d1f8b78fa9b89897b0c19e29311141021b8bd9aa
Marise Hayashi <vyatheslav2707@mail.ru>
2018-08-23 15:29:57 +0300

    Fixes + link open

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const url = require("url");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const open = require('open');
+const url = require('url');
 
-var window = null;
+let mainWindow = null;
 
-app.on('ready', async () => {
-    window = new BrowserWindow({
+function handleRedirect(event, url) {
+    if (url !== mainWindow.webContents.getURL()) {
+        event.preventDefault();
+        open(url);
+    }
+}
+
+function createMainWindow() {
+    let win = new BrowserWindow({
         height: 720,
         width: 1280,
         resizable: true,
         transparent: true
     });
 
-    window.loadURL(url.format({
+    win.loadURL(url.format({
         pathname: path.join(__dirname, 'app', 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
+
+    ['new-window', 'will-navigate'].forEach(eventType => {
+        win.webContents.on(eventType, handleRedirect);
+    });
+
+    win.on('closed', () => { mainWindow = null });
+
+    return win;
+}
+
+app.on('window-all-closed', (event) => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-ipcMain.on('close-main-window', (event) => {
-    app.quit();
+app.on('activate', () => {
+    if (!mainWindow) {
+        mainWindow = createMainWindow();
+    }
+});
+
+app.on('ready', () => {
+    mainWindow = createMainWindow();
 });
